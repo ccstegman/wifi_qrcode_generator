83bbb07b7f33f96b4cc433e6ff98d6264aa66ce6
Chad Stegman <ccstegmantwo@gmail.com>
2023-04-21 08:10:44 -0600

    Added troubleshooting note

--- wifi_qrcode_generator/generate.py
+++ wifi_qrcode_generator/generate.py
@@ -9,5 +9,7 @@ wifi_string = f"WIFI:T:WPA;S:{ssid};P:{password};;"
 # Generate the QR code
 img = qrcode.make(wifi_string)
 
-# Save the QR code to a file
+# Save the QR code to a file.
 img.save("qrcode.png")
+
+# NOTE: THE QR CODE MAY NOT SHOW UP IN THE SOLUTION EXPLORER. YOU MAY HAVE TO OPEN THE FILE EXPLORER ON YOUR DESKTOP AND FIND THE .PNG FILE IN THERE.
